43016159aef65c3b03aa79d8cb755b7fb8bf26e9
aditya-srikanth <adityashrikanth14@gmail.com>
2019-03-01 00:34:43 +0530

    preprocessing done

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,12 @@
 import numpy as np 
-
+import pickle
 def compute_distance(feature_centers,datapoint):
+    '''
+        make changes here!!
+    
+    '''
+
+
     # print(feature_centers.shape,' center ',datapoint.shape,' datapoint')
     return np.sum(np.power(datapoint-feature_centers,2),axis=1)
 
@@ -8,7 +14,7 @@ def kmeans(data,num_cluster_centers,epochs=1000):
     cluster = np.zeros((data.shape[0],1))
     center_indexes = np.random.random_integers(0,data.shape[0],num_cluster_centers)
     feature_centers = data[center_indexes]
-    # print('feat cent: ',feature_centers.shape)    
+    # print('feature centers: ',feature_centers.shape)    
     # print(center_indexes)   
     for epoch in range(epochs):
         distances = np.zeros((num_cluster_centers,1))
@@ -27,7 +33,7 @@ def kmeans(data,num_cluster_centers,epochs=1000):
                 # print('centers: ',feature_centers.shape)
         # if epoch % 10 == 0:        
         #    print('epoch: ',epoch)
-    num_cluster_centers = num_neurons = feature_centers.shape[0]
+    num_cluster_centers = feature_centers.shape[0]
     cluster = np.zeros((data.shape[0],1))
     l2_norm = np.zeros((data.shape[0],1))
     beta = np.zeros((num_cluster_centers,1))
@@ -45,4 +51,46 @@ def kmeans(data,num_cluster_centers,epochs=1000):
         distances_l2 = np.power(l2_norm[cluster_points_indices[:,0]],0.5)
         beta[i] = 1/2*np.power(np.sum(distances_l2)/distances_l2.shape[0],2)
     
-    return feature_centers,beta
+    return feature_centers
+
+def process_dataset(file_path,store=False,output_file_path=None):
+    with open(file_path,'r') as f:
+        textual_data = []
+        protein = ""
+        string = ""
+        max_len = -1
+        for line in f.readlines():
+            # print(line)
+            if ">" in line:
+                continue
+            else:
+                protein += line[:-1]
+                if line[-2] == "*":
+                    protein = protein[:-1]
+                    textual_data += [protein]
+                    max_len = max(max_len,len(protein))
+                    protein = ""
+                elif line[-1] == "*":
+                    protein = protein
+                    textual_data += [protein]
+                    max_len = max(max_len,len(protein))
+                    protein = ""
+                string += protein
+
+        string_list = sorted(list(set(string)))# extract the amino acids
+        mapping_from_acid_to_vector = {acid : string_list.index(acid) for acid in string_list}
+        dataset = np.zeros((len(textual_data),max_len,len(mapping_from_acid_to_vector)))
+        for data_string in range(len(textual_data)):
+            for acid_position in range(len(textual_data[data_string])):
+                dataset[data_string][acid_position][mapping_from_acid_to_vector[textual_data[data_string][acid_position]]] = 1
+        if store:
+            with open('textual_'+output_file_path,'wb') as f:
+                pickle.dump(textual_data,f)
+            with open('matrix_'+output_file_path,'wb') as f:
+                pickle.dump(dataset,f)
+        return textual_data,mapping_from_acid_to_vector,dataset
+
+
+if __name__ == "__main__":
+    # data = process_dataset('dataset.txt',True,'processed_data.txt')
+    textual_data,mapping_from_acid_to_vector,data_matrix = process_dataset('dataset.txt',store=True,output_file_path='data.txt')
